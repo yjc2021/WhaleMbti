697bc8aac5ee291aac5332db8782c39a8fa1c2d9
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-02 23:10:46 +0900

    ✨ App 컴포넌트에 Router 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes/Router";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
