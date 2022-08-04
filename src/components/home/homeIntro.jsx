3458ecdd293464879ff0a485adf6ae23e0866719
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-04 18:39:42 +0900

    ✨ tailwindcss 기반으로 homeIntro 컴포넌트 수정

--- src/components/home/homeIntro.jsx
+++ src/components/home/homeIntro.jsx
@@ -1,31 +1,14 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
 
 const HomeIntro = (props) => {
   return (
-    <Container className="mt-5">
-      <Row>
-        <Col></Col>
-        <Col style={{ textAlign: "center" }} xs={8}>
-          바닷속에서 우연히 만난
-        </Col>
-        <Col></Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col style={{ textAlign: "center" }} xs={9}>
-          아기 고래가 도움을 청해오는데...
-        </Col>
-        <Col></Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col style={{ textAlign: "center" }} xs={8}>
-          나는 어떻게 해야 할까?
-        </Col>
-        <Col></Col>
-      </Row>
-    </Container>
+    /*TODO: 시간에 따라 text display */
+    <div className=" z-10 pt-5 text-white">
+      <div className="text-center">바닷속에서 우연히 만난</div>
+      <div className="text-center">아기 고래가 도움을 청해오는데...</div>
+      <br />
+      <div className="text-center">나는 어떻게 해야 할까?</div>
+    </div>
   );
 };
 
