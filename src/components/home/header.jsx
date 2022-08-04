442b54f104d31758ae0d9295f9a801888b679a02
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-04 18:38:18 +0900

    ✨ tailwindcss 기반 Header 컴포넌트 추가

---
+++ src/components/home/header.jsx
@@ -0,0 +1,141 @@
+import React, { useEffect, useState, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import "./header.css";
+import { useNavigate } from "react-router-dom";
+import styled, { css } from "styled-components";
+const Header = (props) => {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSideBar = () => setIsOpen(!isOpen);
+  const sideBarRef = useRef();
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+      if (
+        isOpen &&
+        sideBarRef.current &&
+        !sideBarRef.current.contains(e.target)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", checkIfClickedOutside);
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [isOpen]);
+
+  return (
+    <header className="sticky z-50 top-0 w-full h-16 bg-blue-950 m-0 px-10 py-2">
+      <nav className="flex items-center justify-center lg:justify-between">
+        <div
+          onClick={() => navigate("/")}
+          className="h-10 w-40 border cursor-pointer"
+        >
+          LOGO
+        </div>
+        <SideBarWrapper
+          isOpen={isOpen}
+          ref={sideBarRef}
+          className="flex flex-col fixed top-0 bottom-0 right-0 right-[-250px] px-2 w-[250px]  overflow-y-auto  bg-white transition-all linear py-4"
+        >
+          <header className="flex flex-col items-center justify-center">
+            {/*
+            <button
+              onClick={toggleSideBar}
+              className="rounded w-10 h-8 bg-blue-200 text-white"
+            >
+              닫기
+            </button>
+            */}
+            <div
+              className="w-40 h-40 rounded-full bg-gray-400 text-white flex justify-center items-center"
+              onClick={() => navigate("/login")}
+            >
+              <FontAwesomeIcon icon={solid("user")} className="w-1/2 h-1/2" />
+            </div>
+            <div className="border-bottom text-sm text-center">
+              로그인이 필요합니다
+            </div>
+          </header>
+          <div className=" my-5 w-full h-50 flex flex-col items-center">
+            <button
+              className="mb-3 w-40 h-10 bg-blue-200 text-white rounded-full cursor-pointer mx-2"
+              onClick={() => navigate("/")}
+            >
+              테스트 기록 보기
+            </button>
+            <button
+              className="mb-3 w-40 h-10 bg-blue-200 text-white rounded-full cursor-pointer mx-2"
+              onClick={() => navigate("/")}
+            >
+              공지사항
+            </button>
+            <button
+              className="mb-3 w-40 h-10 bg-blue-200 text-white rounded-full cursor-pointer mx-2"
+              onClick={() => navigate("/")}
+            >
+              설정
+            </button>
+            <button
+              className="w-40 h-10 bg-blue-200 text-white rounded-full cursor-pointer mx-2"
+              onClick={() => navigate("/")}
+            ></button>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="block">공유하기</span>
+            <div className="p-2 border w-full flex justify-between">
+              <button className="rounded-full w-9 h-9 bg-blue-200"></button>
+              <button className="rounded-full w-9 h-9 bg-blue-200"></button>
+              <button className="rounded-full w-9 h-9 bg-blue-200"></button>
+              <button className="rounded-full w-9 h-9 bg-blue-200"></button>
+              <button className="rounded-full w-9 h-9 bg-blue-200"></button>
+            </div>
+          </div>
+        </SideBarWrapper>
+
+        <div className="hidden lg:flex justify-center w-1/2 ">
+          <span className="mx-2 cursor-pointer" onClick={() => navigate("/")}>
+            테스트 기록 보기
+          </span>
+          <span className="mx-2 cursor-pointer" onClick={() => navigate("/")}>
+            공지사항
+          </span>
+          <span className="mx-2 cursor-pointer" onClick={() => navigate("/")}>
+            설정
+          </span>
+        </div>
+        <span className="hidden lg:block">1,234,567</span>
+        {!isOpen && (
+          <button
+            onClick={toggleSideBar}
+            className="absolute right-0 lg:hidden rounded-full w-10 h-10 bg-blue-200"
+          >
+            <FontAwesomeIcon className="text-white" icon={solid("user")} />
+          </button>
+        )}
+        <div className="hidden lg:flex flex-col items-center">
+          <button className="rounded-full w-10 h-10 bg-blue-200">
+            <FontAwesomeIcon className="text-white" icon={solid("user")} />
+          </button>
+          <span className="inline-block text-xs">로그인</span>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
+
+const SideBarWrapper = styled.div`
+  ${(props) =>
+    props.isOpen &&
+    css`
+      right: 0;
+    `}
+`;
