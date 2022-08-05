d8fd4053a115fa59824229fbcca0ef7af1f9d837
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-06 02:46:47 +0900

    ✨ imgCard에 WhaleSlider 컴포넌트 적용

--- src/components/home/imgCard.jsx
+++ src/components/home/imgCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import WhaleAni from "../animations/whale_ani";
+import WhaleSlider from "./slider";
 
 const ImgCard = (props) => {
   return (
     <div className=" z-10 w-full flex flex-col items-center my-2 border text-white">
-      <WhaleAni />
+      <WhaleSlider />
       <div className="text-3xl ">나는 어떤 고래일까?</div>
     </div>
   );
