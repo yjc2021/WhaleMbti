e5fd3f7bba3f54732391fe3eb189a9a8a86d3935
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-06 15:55:39 +0900

    ✨ TestHome 페이지 라우터 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
-
+import TestHome from "../pages/testHome";
 const Router = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/test" element={<TestHome />} />
       </Routes>
     </>
   );
