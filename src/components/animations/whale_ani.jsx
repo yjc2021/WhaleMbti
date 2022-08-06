b8133592d0089ab34cd7995fa5d6bc748f5f06e2
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-06 16:01:17 +0900

    ✨ whaleAni 컴포넌트 recoilState 적용 및 transition 추가

--- src/components/animations/whale_ani.jsx
+++ src/components/animations/whale_ani.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRecoilValue } from "recoil";
 import whaleGif from "../../animations/whale.gif";
-const WhaleAni = (props) => {
+import { swimAtom } from "../../atoms";
+const WhaleAni = () => {
+  const swim = useRecoilValue(swimAtom);
+
   return (
-    <>
-      <img src={whaleGif} alt="whale" className="border h-64 w-auto" />
-    </>
+    <div
+      swim={swim}
+      className={`absolute rotate-12 transition-translate duration-[4000ms] ease-linear ${
+        swim && "translate-x-[800px] translate-y-[200px]"
+      } left-[-400px] top-[350px] lg:left-[250px] lg:top-[300px]  w-[350px] h-auto z-1 `}
+    >
+      {swim && <img src={whaleGif} alt="whale" />}
+    </div>
   );
 };
 
