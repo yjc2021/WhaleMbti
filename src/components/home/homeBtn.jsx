9cd2e5cbfeb3e36bccc0151d607d3b7ae5d3ab19
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-06 16:00:41 +0900

    ✨ HomeBtn 컴포넌트 swim recoilState 적용

--- src/components/home/homeBtn.jsx
+++ src/components/home/homeBtn.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import { swimAtom } from "../../atoms";
+import { useRecoilState } from "recoil";
 const HomeBtn = (props) => {
+  const [swim, setSwim] = useRecoilState(swimAtom);
+  const navigate = useNavigate();
+  const handleClick = () => {
+    setSwim(true);
+    setTimeout(() => navigate("/test"), 4000);
+  };
+
   return (
     <div className=" z-10 mt-5 mb-4 w-full h-32 flex items-center justify-center">
-      <button className="rounded-full w-60 h-14 bg-gray-300 text-black text-xl ">
+      <button
+        onClick={handleClick}
+        className="rounded-full w-60 h-14 bg-gray-300 text-black text-xl "
+      >
         아기 고래 만나러 가기
       </button>
     </div>
