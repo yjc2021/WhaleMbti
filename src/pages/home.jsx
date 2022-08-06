72759d994f0632d0f20eb164dea3534ec4dffc40
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-06 16:03:26 +0900

    ✨ Home 페이지에 WhaleAni, Footer 컴포넌트 추가

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,17 +6,20 @@ import HomeBtn from "../components/home/homeBtn";
 import Share from "../components/home/share";
 import Stat from "../components/home/stat";
 import Bubbles from "../components/animations/bubbles";
+import Footer from "../components/home/footer";
+import WhaleAni from "../components/animations/whale_ani";
 
 const Home = (props) => {
   return (
-    <div className="relative h-screen flex flex-col items-center overflow-y-auto bg-blue-950 pb-5 text-white ">
+    <div className="relative h-screen flex flex-col items-center overflow-y-auto bg-blue-950 pb-5 lg:pb-0 text-white ">
       <Header />
       <ImgCard />
       <HomeIntro />
+      <WhaleAni />
       <HomeBtn />
       <Stat />
       <Bubbles />
-      <Share />
+      <Footer />
     </div>
   );
 };
