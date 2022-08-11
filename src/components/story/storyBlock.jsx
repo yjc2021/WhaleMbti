568f40ca534c3c37e814c87ee2c3acdb4d0d1bf4
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-11 11:54:55 +0900

    ✨ StoryBlock 컴포넌트 생성

---
+++ src/components/story/storyBlock.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StoryBlock = ({ story }) => {
+  return (
+    <div className="h-64 flex flex-col justify-between">
+      <img src="#" className="w-full h-52 border-2 border-gray-700" />
+      <div className="text-center">{story}</div>
+    </div>
+  );
+};
+
+export default StoryBlock;
