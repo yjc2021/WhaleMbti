4b2dec7a897a291458b74ccc86851f8579834f3e
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-11 11:49:58 +0900

    🎨  Home 페이지 newHome 배경화면 추가

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,25 +3,28 @@ import Header from "../components/home/header";
 import ImgCard from "../components/home/imgCard";
 import HomeIntro from "../components/home/homeIntro";
 import HomeBtn from "../components/home/homeBtn";
-import Share from "../components/home/share";
 import Stat from "../components/home/stat";
-import Bubbles from "../components/animations/bubbles";
 import Footer from "../components/home/footer";
 import WhaleAni from "../components/animations/whale_ani";
+import styled from "styled-components";
+import newHome from "../animations/newHome.jpg";
 
 const Home = (props) => {
   return (
-    <div className="relative h-screen flex flex-col items-center overflow-y-auto bg-blue-950 pb-5 lg:pb-0 text-white ">
+    <Wrapper className="relative h-screen flex flex-col items-center overflow-y-auto bg-blue-950 pb-5 lg:pb-0 text-white bg-no-repeat bg-center bg-cover">
       <Header />
       <ImgCard />
       <HomeIntro />
       <WhaleAni />
       <HomeBtn />
       <Stat />
-      <Bubbles />
       <Footer />
-    </div>
+    </Wrapper>
   );
 };
 
 export default Home;
+
+const Wrapper = styled.div`
+  background-image: url(${newHome});
+`;
