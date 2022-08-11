fa00ee4cc6408368d5c6e4e8a82390ddf9bcd694
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-11 11:50:48 +0900

    ✨ testHome 컴포넌트 Header 및 StartBtn 추가

--- src/pages/testHome.jsx
+++ src/pages/testHome.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "../components/home/header";
+import StartBtn from "../components/story/startBtn";
 
-const TestHome = (props) => {
-  return <></>;
+const TestHome = () => {
+  return (
+    <>
+      <Header />
+      <StartBtn />
+    </>
+  );
 };
 
 export default TestHome;
