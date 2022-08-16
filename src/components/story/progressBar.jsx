61b534ec61687e3ec057f4792d3479758049caf2
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-16 15:42:37 +0900

    ✨ progressBar 컴포넌트 생성

---
+++ src/components/story/progressBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+const ProgressBar = ({ id }) => {
+  return (
+    <>
+      <div className="hidden lg:flex w-full h-[20px]  items-center rounded-full bg-[white] fixed bottom-3">
+        <HighLight width={(id / 12) * 100 + "%"}></HighLight>
+        <Dot />
+      </div>
+    </>
+  );
+};
+
+export default ProgressBar;
+
+const HighLight = styled.div`
+  background: skyblue;
+  height: 20px;
+  width: ${(props) => props.width};
+  transition: width 1s;
+  border-radius: 10px;
+`;
+
+const Dot = styled.div`
+  background: #fff;
+  border: 5px solid skyblue;
+  box-sizing: border-box;
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  margin: 0px 0px 0px -10px;
+`;
