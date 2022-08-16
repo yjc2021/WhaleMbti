fdec5827e57a3e12ac47f09f347f71ae3718507b
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-16 15:28:32 +0900

    ✨ test 데이터 설정

--- src/data.js
+++ src/data.js
@@ -1,3 +1,42 @@
-export const storyData = ["hi", "this", "is", "just", "a", "test"];
-export const decisionAData = ["d1", "d2", "d3"];
-export const decisionBData = ["d4", "d5", "d6"];
+export const storyData = [
+  "S1",
+  "S2",
+  "S3",
+  "S4",
+  "S5",
+  "S6",
+  "S7",
+  "S8",
+  "S9",
+  "S10",
+  "S11",
+  "S12",
+];
+export const decisionAData = [
+  "A1",
+  "A2",
+  "A3",
+  "A4",
+  "A5",
+  "A6",
+  "A7",
+  "A8",
+  "A9",
+  "A10",
+  "A11",
+  "A12",
+];
+export const decisionBData = [
+  "B1",
+  "B2",
+  "B3",
+  "B4",
+  "B5",
+  "B6",
+  "B7",
+  "B8",
+  "B9",
+  "B10",
+  "B11",
+  "B12",
+];
