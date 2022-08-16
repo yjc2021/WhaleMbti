9a9d03f4a174022a0e7ce22ed86450c496c76be8
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-16 15:35:24 +0900

    ✨ whaleImg 컴포넌트 JSX 추가

--- src/components/result/whaleImg.jsx
+++ src/components/result/whaleImg.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const WhaleImg = (props) => {
-  return <div></div>;
+  return (
+    <div className="border w-[350px] h-[400px]">
+      <img alt="WhaleImage" />
+    </div>
+  );
 };
 export default WhaleImg;
