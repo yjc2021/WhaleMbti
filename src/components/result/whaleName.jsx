e58fa0effcf702938df31ff5d60ad9d8b382d408
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-16 15:35:44 +0900

    ✨ whaleName JSX 추가

--- src/components/result/whaleName.jsx
+++ src/components/result/whaleName.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const WhaleName = (props) => {
-  return <div></div>;
+const WhaleName = ({ whaleName }) => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="text-sm">내가 고래라면</div>
+      {whaleName ? (
+        <div>
+          <div>{whaleName.description}</div>
+          <div>{whaleName.name}</div>
+        </div>
+      ) : (
+        <div className="text-center">
+          <div>낭만 넘치는 바닷 속의 음악가</div>
+          <div className="text-2xl">흑동고래</div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default WhaleName;
