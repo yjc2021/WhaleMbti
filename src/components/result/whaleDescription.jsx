6f95d45d39d791027fcd5c5c9a94c4b016cdc04d
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-16 15:31:49 +0900

    ✨ whaleDescription 컴포넌트 생성

--- src/components/result/whaleDescription.jsx
+++ src/components/result/whaleDescription.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const WhaleDescription = (props) => {
-  return <div></div>;
+  return (
+    <div className="border w-full flex flex-col">
+      <div className="border w-full lg:w-3/4 h-[300px]">
+        <span>흑동고래가 나온 당신은</span>
+        <p>본문</p>
+      </div>
+      <div className="border w-full flex flex-col lg:flex-row ">
+        <div className="border w-full lg:w-3/4 h-[300px]">
+          <span>흑동고래는</span>
+          <p>본문</p>
+        </div>
+        <img className=" w-[300px] h-[300px] border" alt="WhaleDescription" />
+      </div>
+    </div>
+  );
 };
 
 export default WhaleDescription;
