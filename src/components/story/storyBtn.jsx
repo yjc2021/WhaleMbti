8a0928e057a31f3fa8a8f2ef0e17dbfeed8cb2fe
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-21 22:38:24 +0900

    🐛 mbti cnt 계산 버그 수정 및 test page수 수정

--- src/components/story/storyBtn.jsx
+++ src/components/story/storyBtn.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
-const LAST_PAGE = 12;
+const LAST_PAGE = 13;
 const StoryBtn = ({ cntD, id, type, decision }) => {
   const navigate = useNavigate();
   const mbtiD = useRef({
@@ -20,9 +20,9 @@ const StoryBtn = ({ cntD, id, type, decision }) => {
         case 0:
           for (let i = 0; i < 3; i++) {
             if (data[0].data[i] === "a") {
-              mbtiD.current.eCnt++;
-            } else if (data[0].data[i] === "b") {
               mbtiD.current.iCnt++;
+            } else if (data[0].data[i] === "b") {
+              mbtiD.current.eCnt++;
             }
           }
           break;
@@ -61,22 +61,22 @@ const StoryBtn = ({ cntD, id, type, decision }) => {
   const activate = (id) => {
     // TODO: 점수 가산하기
     console.log(id);
+    id === LAST_PAGE && getMbtiD(cntD.current);
+    id === LAST_PAGE
+      ? navigate("/test/result", { state: mbtiD })
+      : navigate(`/test/story/${parseInt(id) + 1}`);
     type === "a"
       ? (cntD.current[Math.floor((id - 1) / 3)].data[(id - 1) % 3] = "a")
       : (cntD.current[Math.floor((id - 1) / 3)].data[(id - 1) % 3] = "b");
 
     console.log(cntD.current[Math.floor((id - 1) / 3)].data);
-    id === LAST_PAGE && getMbtiD(cntD.current);
-    id === LAST_PAGE
-      ? navigate("/test/result", { state: mbtiD })
-      : navigate(`/test/story/${parseInt(id) + 1}`);
   };
   return (
     <button
       onClick={() => activate(id)}
       className="w-full h-16 bg-gray-200 rounded-full"
     >
-      {parseInt(id) !== LAST_PAGE ? decision : "나의 정체 확인하러 가기"}
+      {id !== LAST_PAGE ? decision : "나의 정체 확인하러 가기"}
     </button>
   );
 };
