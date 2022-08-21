bb1f24e8a5c0d8f0b7dd62f47cd01c6e30037848
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-21 22:30:14 +0900

    ✨ data.js 업데이트

--- src/data.js
+++ src/data.js
@@ -1,3 +1,20 @@
+import a1 from "./animations/고양이고래(ENTJ).png";
+import a2 from "./animations/귀신고래(ISTP).png";
+import a3 from "./animations/남방참고래(ESFJ).png";
+import a4 from "./animations/남방큰돌고래(ENFJ).png";
+import a5 from "./animations/낫돌고래(ESTP).png";
+import a6 from "./animations/대왕고래(INFJ).png";
+import a7 from "./animations/민부리고래(ISTJ).png";
+import a8 from "./animations/밍크고래(ISFJ).png";
+import a9 from "./animations/범고래(ESTJ).png";
+import a10 from "./animations/벨루가(ENFP).png";
+import a11 from "./animations/병코돌고래(ENTP).png";
+import a12 from "./animations/북극고래(INFP).png";
+import a13 from "./animations/상괭이(INTP).png";
+import a14 from "./animations/일각고래(INTJ).png";
+import a15 from "./animations/향유고래(ISFP).png";
+import a16 from "./animations/혹등고래(ESFP).png";
+
 export const storyData = [
   "S1",
   "S2",
@@ -13,30 +30,227 @@ export const storyData = [
   "S12",
 ];
 export const decisionAData = [
-  "A1",
-  "A2",
-  "A3",
-  "A4",
-  "A5",
-  "A6",
-  "A7",
-  "A8",
-  "A9",
-  "A10",
-  "A11",
-  "A12",
+  "i",
+  "i",
+  "i",
+  "s",
+  "s",
+  "s",
+  "t",
+  "t",
+  "t",
+  "p",
+  "p",
+  "p",
 ];
 export const decisionBData = [
-  "B1",
-  "B2",
-  "B3",
-  "B4",
-  "B5",
-  "B6",
-  "B7",
-  "B8",
-  "B9",
-  "B10",
-  "B11",
-  "B12",
+  "e",
+  "e",
+  "e",
+  "n",
+  "n",
+  "n",
+  "f",
+  "f",
+  "f",
+  "j",
+  "j",
+  "j",
+];
+export const whaleIndex = [
+  {
+    id: 1,
+    whaleName: "일각고래",
+    whaleTitle: "일각고래 한줄",
+    whaleImg: a14,
+  },
+  {
+    whaleId: 2,
+    whaleName: "상괭이",
+    whaleTitle: "상괭이 한줄",
+    whaleImg: a13,
+  },
+  {
+    whaleId: 3,
+    whaleName: "고양이고래",
+    whaleTitle: "고양이고래 한줄",
+    whaleImg: a1,
+  },
+  {
+    whaleId: 4,
+    whaleName: "병코돌고래",
+    whaleTitle: "병코돌고래 한줄",
+    whaleImg: a11,
+  },
+  {
+    whaleId: 5,
+    whaleName: "대왕고래",
+    whaleTitle: "대왕고래 한줄",
+    whaleImg: a6,
+  },
+  {
+    whaleId: 6,
+    whaleName: "북극고래",
+    whaleTitle: "북극고래 한줄",
+    whaleImg: a12,
+  },
+  {
+    whaleId: 7,
+    whaleName: "남방큰돌고래",
+    whaleTitle: "남방큰돌고래 한줄",
+    whaleImg: a4,
+  },
+  {
+    whaleId: 8,
+    whaleName: "벨루가",
+    whaleTitle: "벨루가 한줄",
+    whaleImg: a10,
+  },
+  {
+    whaleId: 9,
+    whaleName: "민부리고래",
+    whaleTitle: "민부리고래 한줄",
+    whaleImg: a7,
+  },
+  {
+    whaleId: 10,
+    whaleName: "밍크고래",
+    whaleTitle: "밍크고래 한줄",
+    whaleImg: a8,
+  },
+  {
+    whaleId: 11,
+    whaleName: "범고래",
+    whaleTitle: "범고래 한줄",
+    whaleImg: a9,
+  },
+  {
+    whaleId: 12,
+    whaleName: "남방참고래",
+    whaleTitle: "남방참고래 한줄",
+    whaleImg: a3,
+  },
+  {
+    whaleId: 13,
+    whaleName: "귀신고래",
+    whaleTitle: "귀신고래 한줄",
+    whaleImg: a2,
+  },
+  {
+    whaleId: 14,
+    whaleName: "항유고래",
+    whaleTitle: "항유고래 한줄",
+    whaleImg: a15,
+  },
+  {
+    whaleId: 15,
+    whaleName: "낫돌고래",
+    whaleTitle: "낫돌고래 한줄",
+    whaleImg: a5,
+  },
+  {
+    whaleId: 16,
+    whaleName: "혹등고래",
+    whaleTitle: "혹등고래 한줄",
+    whaleImg: a16,
+  },
+];
+
+export const whaleDescription = [
+  {
+    id: 1,
+    whaleName: "일각고래",
+    whaleDesTop: "일각고래 본문1",
+    whaleDesLow: "일각고래 본문2",
+  },
+  {
+    whaleId: 2,
+    whaleName: "상괭이",
+    whaleDesTop: "일각고래 본문1",
+    whaleDesLow: "일각고래 본문2",
+  },
+  {
+    whaleId: 3,
+    whaleName: "고양이고래",
+    whaleDesTop: "고양이고래 본문1",
+    whaleDesLow: "고양이고래 본문2",
+  },
+  {
+    whaleId: 4,
+    whaleName: "병코돌고래",
+    whaleDesTop: "병코돌고래 본문1",
+    whaleDesLow: "병코돌고래 본문2",
+  },
+  {
+    whaleId: 5,
+    whaleName: "대왕고래",
+    whaleDesTop: "대왕고래 본문1",
+    whaleDesLow: "대왕고래 본문2",
+  },
+  {
+    whaleId: 6,
+    whaleName: "북극고래",
+    whaleDesTop: "북극고래 본문1",
+    whaleDesLow: "북극고래 본문2",
+  },
+  {
+    whaleId: 7,
+    whaleName: "남방큰돌고래",
+    whaleDesTop: "남방큰돌고래 본문1",
+    whaleDesLow: "남방큰돌고래 본문2",
+  },
+  {
+    whaleId: 8,
+    whaleName: "벨루가",
+    whaleDesTop: "벨루가 본문1",
+    whaleDesLow: "벨루가 본문2",
+  },
+  {
+    whaleId: 9,
+    whaleName: "민부리고래",
+    whaleDesTop: "민부리고래 본문1",
+    whaleDesLow: "민부리고래 본문2",
+  },
+  {
+    whaleId: 10,
+    whaleName: "밍크고래",
+    whaleDesTop: "밍크고래 본문1",
+    whaleDesLow: "밍크고래 본문2",
+  },
+  {
+    whaleId: 11,
+    whaleName: "범고래",
+    whaleDesTop: "범고래 본문1",
+    whaleDesLow: "범고래 본문2",
+  },
+  {
+    whaleId: 12,
+    whaleName: "남방참고래",
+    whaleDesTop: "남방참고래 본문1",
+    whaleDesLow: "남방참고래 본문2",
+  },
+  {
+    whaleId: 13,
+    whaleName: "귀신고래",
+    whaleDesTop: "귀신고래 본문1",
+    whaleDesLow: "귀신고래 본문2",
+  },
+  {
+    whaleId: 14,
+    whaleName: "항유고래",
+    whaleDesTop: "항유고래 본문1",
+    whaleDesLow: "항유고래 본문2",
+  },
+  {
+    whaleId: 15,
+    whaleName: "낫골고래",
+    whaleDesTop: "낫골고래 본문1",
+    whaleDesLow: "낫골고래 본문2",
+  },
+  {
+    whaleId: 16,
+    whaleName: "혹등고래",
+    whaleDesTop: "혹등고래 본문1",
+    whaleDesLow: "혹등고래 본문2",
+  },
 ];
