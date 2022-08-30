7d588d3be5615b1cdadb23279fce4a0fc4682821
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-30 12:38:49 +0900

    ✨ data.js 실제 스토리 텍스트 적용

--- src/data.js
+++ src/data.js
@@ -15,47 +15,138 @@ import a14 from "./animations/일각고래(INTJ).png";
 import a15 from "./animations/향유고래(ISFP).png";
 import a16 from "./animations/혹등고래(ESFP).png";
 
+import s1_2 from "./img/story/1-1.jpg";
+import s1_1 from "./img/story/1-2.jpg";
+import s2 from "./img/story/2.jpg";
+import s3 from "./img/story/3.jpg";
+import s4 from "./img/story/4.jpg";
+import s5 from "./img/story/5.jpg";
+import s6 from "./img/story/6.jpg";
+import s7 from "./img/story/7.jpg";
+import s8 from "./img/story/8.jpg";
+import s9 from "./img/story/9.jpg";
+import s10 from "./img/story/10.jpg";
+import s11 from "./img/story/11.jpg";
+import s12 from "./img/story/12.jpg";
+import s13 from "./img/story/13.jpg";
+import s14 from "./img/story/14.jpg";
+import s15 from "./img/story/15.jpg";
+import s16 from "./img/story/16.jpg";
+import s17 from "./img/story/17.jpg";
+import s18 from "./img/story/18.jpg";
+import s19 from "./img/story/19.jpg";
+import s20 from "./img/story/20.jpg";
+import s21 from "./img/story/21.jpg";
+import s22 from "./img/story/22.jpg";
+import s23 from "./img/story/23.jpg";
+import s24 from "./img/story/24.jpg";
+import s25 from "./img/story/25.jpg";
+import s26 from "./img/story/26.jpg";
+import s27 from "./img/story/27.jpg";
+
+export const introImg = [
+  {
+    id: 1,
+    url: s1_2,
+  },
+  {
+    id: 2,
+    url: s2,
+  },
+  {
+    id: 3,
+    url: s3,
+  },
+];
+export const storyImg = [
+  [s4],
+  [s5],
+  [s6],
+  [s7],
+  [s8],
+  [s9],
+  [s10],
+  [s11],
+  [s12, s13],
+  [s14, s15],
+  [s15],
+  [s16],
+  [s17],
+  [s18, s19],
+  [s20],
+  [s21],
+  [s22],
+  [s23, s23, s24, s25],
+  [s26, s27],
+];
+
+export const introData = [
+  "문득 눈을 뜨니",
+  "나는 고래가 되어 바다 한 가운데 있었다.",
+  "이때 저 멀리서 작은 생명체를 발견하는데...",
+];
 export const storyData = [
-  "S1",
-  "S2",
-  "S3",
-  "S4",
-  "S5",
-  "S6",
-  "S7",
-  "S8",
-  "S9",
-  "S10",
-  "S11",
-  "S12",
+  "저기 웬 아기 고래가 혼자 있다. 나는 어떻게 할까?",
+  "가까이 가보니 아기 고래는 엉엉 울고 있었다.",
+  "얘기를 들어보니, 엄마가 사라졌다고 한다. 어떻게 하지?",
+  "열심히 헤엄치던 중,\n 아기 고래가 걱정스러운 목소리로 말한다.|우리 아빠가 인간들로부터 저를 구하려다 돌아가셨어요...|“제가 미워서 엄마가 저를 버린 걸까요...?” 이 말을 들은 나는",
+  "아기 고래를 달래며 가고 있는데, 아기 고래가 갑자기 멈춰선다.",
+  "“잠시만요! 이거 우리 엄마 목걸이에요!” 이 말을 들은 나는 생각한다",
+  "그 순간, 해수면 위에서 커다란 뱃고동 소리가 들려온다.",
+  "아기 고래가 화들짝 놀라며 기겁을 한다. “인간들이 또 나타났나봐요!”",
+  "아기 고래가 위험할까 우선 혼자서 올라가 보기로 했다.|얼마나 올라갔을까 저 멀리 무언가가 보인다.",
+  "그물에 걸린 어미 고래였다.|숨 쉬기 힘들어 하는 거 같은데...",
+  "어떻게 하면 좋을까?",
+  "이때 지나던 물고기 무리가 다가와 도와주겠다고 한다. 이때 나는",
+  "기다리고 있는 아기 고래에게 가서 상황을 알려야겠다. 이때 나는",
+  "잠시 후 아기 고래와 물고기 친구들이 힘을 합쳐 그물을 끊어냈다!|어미 고래와 아기 고래는 서로를 껴안고 눈물을 흘린다.",
+  "어미 고래는 나에게 다가와 고맙다는 인사를 건넨다.",
+  "아기 고래와 엄마 고래가 집으로 돌아갔다.",
+  "혼자서 돌아가는 길... 이때 나는",
+  '아기 고래와 헤어지고 얼마를 헤엄쳤을까,|누군가 나를 부르는 소리가 들린다.|아기 고래다.|"이거 선물이에요. 저희 엄마를 구해주셔서 감사해요"',
+  "아기 고래가 건네준 건 작은 거울이었다.|거울 속 내 모습은...?",
 ];
 export const decisionAData = [
-  "i",
-  "i",
-  "i",
-  "s",
-  "s",
-  "s",
-  "t",
-  "t",
-  "t",
-  "p",
-  "p",
-  "p",
+  "다가가서 말을 걸어본다.",
+  "무슨 일이 있나 걱정스러워 안절부절한다.",
+  "일단 아기 고래를 데리고 출발한다.",
+  "그런 걱정하지 말라며 다독인다.",
+  "",
+  "'여기 떨어트린 걸 보니 이 길을 지나갔나봐!'",
+  "",
+  "일단 해수면 쪽으로 계속 나아간다.",
+  "",
+  "",
+  "주위에 도움을 요청하러 간다.",
+  "물고기들이 그물을 끊어낼 수 있을지 조금 걱정이 된다.",
+  "아기 고래가 놀라지 않도록 고민하고 말을 한다.",
+  "",
+  "내가 아기 고래를 어디서 만났고, 어떻게 이곳에 왔는지 신이 나서 말해준다.",
+  "짧은 시간이었지만 정이 많이 든 아기 고래와 헤어지려니 눈물이 날 것 같다",
+  "고래들이 집에 무사히 들어가기를 바라며 간다.",
+  "",
+  "",
 ];
 export const decisionBData = [
-  "e",
-  "e",
-  "e",
-  "n",
-  "n",
-  "n",
-  "f",
-  "f",
-  "f",
-  "j",
-  "j",
-  "j",
+  "날 무서워할까 조심스레 다가가본다.",
+  "갑자기 왜 울지...? 당황스럽다.",
+  "마지막으로 엄마와 헤어진 장소를 찾아가 본다.",
+  "그때 다친 상처는 괜찮은지 물어본다.",
+  "",
+  "‘혹시..나쁜 일을 당하신 건 아니겠지?’ 불길한 상상들이 떠오른다.",
+  "",
+  "우선 그 자리에 멈춰서 어떻게 할지 생각하고 간다.",
+  "",
+  "",
+  "일단 혼자서 어떻게든 그물을 끊어보려고 한다.",
+  "선뜻 도와준다는 물고기들이 든든한 아군처럼 느껴진다.",
+  "일단 가서 본 장면을 횡성수설 얘기한다.",
+  "",
+  "수줍게 웃으며 다행이라는 말을 건넨다.",
+  "엄마를 찾아줬다는 생각에 뿌듯함으로 가득하다.",
+  "고래들이 헤쳐나가야 할 험난한 미래를 걱정하며 간다.",
+  "",
+  "",
 ];
 export const whaleIndex = [
   {
@@ -138,8 +229,8 @@ export const whaleIndex = [
   },
   {
     whaleId: 14,
-    whaleName: "항유고래",
-    whaleTitle: "항유고래 한줄",
+    whaleName: "향유고래",
+    whaleTitle: "향유고래 한줄",
     whaleImg: a15,
   },
   {
@@ -237,9 +328,9 @@ export const whaleDescription = [
   },
   {
     whaleId: 14,
-    whaleName: "항유고래",
-    whaleDesTop: "항유고래 본문1",
-    whaleDesLow: "항유고래 본문2",
+    whaleName: "향유고래",
+    whaleDesTop: "향유고래 본문1",
+    whaleDesLow: "향유고래 본문2",
   },
   {
     whaleId: 15,
