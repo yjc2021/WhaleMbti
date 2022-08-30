57e501fcf4e9b0814f0205b98b1d41eeb6712cb1
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-30 13:07:57 +0900

    ✨ testStory 컴포넌트 mbti 계산 로직 수정

--- src/pages/testStory.jsx
+++ src/pages/testStory.jsx
@@ -5,11 +5,13 @@ import StoryBlock from "../components/story/storyBlock";
 import StoryHeader from "../components/story/storyHeader";
 import ProgressBar from "../components/story/progressBar";
 import { storyData, decisionAData, decisionBData } from "../data";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useRef } from "react";
-const LAST_PAGE = 13;
+import styled, { css } from "styled-components";
+const LAST_PAGE = 19;
 
 const TestStory = () => {
+  const navigate = useNavigate();
   const cntD = useRef([
     { id: 0, data: { 0: "z", 1: "z", 2: "z" } },
     { id: 1, data: { 0: "z", 1: "z", 2: "z" } },
@@ -20,6 +22,7 @@ const TestStory = () => {
   const story = storyData[storyId - 1];
   const decisionA = decisionAData[storyId - 1];
   const decisionB = decisionBData[storyId - 1];
+  console.log(decisionA, decisionB);
   if (storyId > LAST_PAGE || !storyId) {
     return <div>존재하지 않는 페이지입니다</div>;
   }
@@ -27,23 +30,31 @@ const TestStory = () => {
     <>
       <Header />
       <StoryHeader id={storyId} />
-      <div className="px-5">
-        <StoryBlock story={story} />
+      <div className="px-5 bg-black h-full font-semibold pt-5 flex-col">
+        <StoryBlock id={storyId} story={story} />
         {parseInt(storyId) !== LAST_PAGE ? (
-          <div className="border-2 border-gray-700 h-36 my-8 flex flex-col justify-between">
-            <StoryBtn
-              id={parseInt(storyId)}
-              type="a"
-              decision={decisionA}
-              cntD={cntD}
-            />
-            <StoryBtn
-              id={parseInt(storyId)}
-              type="b"
-              decision={decisionB}
-              cntD={cntD}
-            />
-          </div>
+          <ButtonWrapper
+            id={parseInt(storyId)}
+            className="border-2 border-gray-700 h-36 mt-10 flex flex-col justify-between "
+            onClick={() => navigate(`/test/story/${parseInt(storyId) + 1}`)}
+          >
+            {decisionA[parseInt(storyId)] && (
+              <StoryBtn
+                id={parseInt(storyId)}
+                type="a"
+                decision={decisionA}
+                cntD={cntD}
+              />
+            )}
+            {decisionB[parseInt(storyId)] && (
+              <StoryBtn
+                id={parseInt(storyId)}
+                type="b"
+                decision={decisionB}
+                cntD={cntD}
+              />
+            )}
+          </ButtonWrapper>
         ) : (
           <div className="border-2 border-gray-700 h-36 my-8 flex items-end">
             <StoryBtn id={parseInt(storyId)} cntD={cntD} />
@@ -56,3 +67,11 @@ const TestStory = () => {
 };
 
 export default TestStory;
+
+const ButtonWrapper = styled.div`
+  ${(props) =>
+    props.id === 4 &&
+    css`
+      margin-top: 9rem;
+    `}
+`;
