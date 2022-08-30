e165c5d121cd3d582022c8c13ccac11fb0e5e37f
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-30 13:21:58 +0900

    ✨ testHome 컴포넌트 IntroBlock  및 timeout 로직 적용

--- src/pages/testHome.jsx
+++ src/pages/testHome.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import Header from "../components/home/header";
+import IntroBlock from "../components/story/introBlock";
 import StartBtn from "../components/story/startBtn";
+import { introData } from "../data";
+import { useSetRecoilState } from "recoil";
+import { counterAtom } from "../atoms";
 
 const TestHome = () => {
+  const setCounter = useSetRecoilState(counterAtom);
+  const introId = useRef(0);
+  const [id, setId] = useState(0);
+  const addId = () => {
+    console.log(introId.current, id);
+
+    if (introId.current < 2) {
+      setId((cur) => cur + 1);
+      introId.current += 1;
+      setTimeout(addId, 2000);
+    }
+  };
+  useEffect(() => {
+    setTimeout(() => addId(), 2000);
+  }, []);
+  useEffect(() => {
+    setCounter(1);
+  });
   return (
-    <>
+    <div className="text-white h-full ">
       <Header />
-      <StartBtn />
-    </>
+      <div className="font-semibold px-5 pt-5 bg-black h-full">
+        <IntroBlock id={id} story={introData[id]} />
+        <StartBtn />
+      </div>
+    </div>
   );
 };
 
