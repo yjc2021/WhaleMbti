a5b0265d122b561cf9b2104a72769e81c5edb966
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-30 12:59:53 +0900

    ✨ story 컴포넌트 리코일 적용 및 mbti 계산 로직 수정

--- src/components/story/storyBlock.jsx
+++ src/components/story/storyBlock.jsx
@@ -1,10 +1,81 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { storyImg } from "../../data";
+import s2 from "../../img/story/2.jpg";
+const StoryBlock = ({ id, story }) => {
+  let scriptTimerId;
+  let imgTimerId;
+  const { storyId } = useParams();
+  //const scriptTmp = useRef(story.split("|"));
+  //const [scriptTmp, setTmp] = useState(story.split("|"));
+  const scriptTmp = story.split("|");
+  const imgTmp = storyImg[storyId - 1];
+  console.log(scriptTmp);
+  console.log(imgTmp);
+  const idx = useRef(0);
+  const posterIdx = useRef(0);
+  const [script, setScript] = useState([]);
+  const [poster, setPoster] = useState([]);
 
-const StoryBlock = ({ story }) => {
+  const makeScript = () => {
+    if (scriptTmp.length > idx.current) {
+      setScript((cur) => [...cur, scriptTmp[idx.current++]]);
+      scriptTimerId = setTimeout(makeScript, 2000);
+    }
+  };
+  const makePoster = () => {
+    console.log("makePoster");
+    console.log(`imgTmp: ${imgTmp.length}`);
+    console.log(`posterIdx: ${posterIdx.current}`);
+    if (imgTmp.length > posterIdx.current) {
+      console.log(`inside if`);
+      setPoster(imgTmp[posterIdx.current++]);
+      imgTimerId = setTimeout(makePoster, 2000);
+    }
+  };
+  useEffect(() => {
+    idx.current = 0;
+    setScript([]);
+    let cnt = scriptTmp.length;
+    if (cnt <= 1) {
+      setScript((cur) => [...cur, scriptTmp]);
+      return;
+    }
+    //setTimeout(makeScript, 2000);
+    makeScript();
+    return () => {
+      clearTimeout(scriptTimerId);
+    };
+  }, [storyId]);
+  useEffect(() => {
+    posterIdx.current = 0;
+    console.log("setPoster()");
+    setPoster("");
+    let posterCnt = imgTmp.length;
+    if (posterCnt <= 1) {
+      setPoster(imgTmp);
+      return;
+    }
+    makePoster();
+    if (imgTimerId)
+      return () => {
+        clearTimeout(imgTimerId);
+      };
+  }, [storyId]);
   return (
     <div className="h-64 flex flex-col justify-between">
-      <img src="" className="w-full h-52 border-2 border-gray-700" />
-      <div className="text-center">{story}</div>
+      <img
+        src={poster ? poster : s2}
+        className="w-[333px] h-[225px] border-2 border-gray-700"
+        alt="story"
+      />
+      <div className="text-center text-white">
+        {script.map((val) => (
+          <div>{val}</div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/story/storyBtn.jsx
+++ src/components/story/storyBtn.jsx
@@ -1,9 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { useRecoilState } from "recoil";
+import { counterAtom } from "../../atoms";
 const LAST_PAGE = 13;
 const StoryBtn = ({ cntD, id, type, decision }) => {
   const navigate = useNavigate();
+  const [counter, setCounter] = useRecoilState(counterAtom);
   const mbtiD = useRef({
     eCnt: 0,
     iCnt: 0,
@@ -14,69 +16,130 @@ const StoryBtn = ({ cntD, id, type, decision }) => {
     pCnt: 0,
     jCnt: 0,
   });
+  //const [mbtiState, setMbtiState] = useRecoilState(mbtiAtom);
+  const resetMbtiD = () => {
+    return {
+      eCnt: 0,
+      iCnt: 0,
+      nCnt: 0,
+      sCnt: 0,
+      tCnt: 0,
+      fCnt: 0,
+      pCnt: 0,
+      jCnt: 0,
+    };
+  };
+  /* const resetMbtiState = () => {
+    setMbtiState({
+      eCnt: 0,
+      iCnt: 0,
+      nCnt: 0,
+      sCnt: 0,
+      tCnt: 0,
+      fCnt: 0,
+      pCnt: 0,
+      jCnt: 0,
+    });
+  };*/
   const getMbtiD = (data) => {
+    console.log("data");
+    console.log(data);
+    console.log("before reset");
+    console.log(mbtiD);
+    mbtiD.current = resetMbtiD();
+    console.log("after reset");
+    console.log(mbtiD);
     for (let a = 0; a < 4; a++) {
       switch (a) {
         case 0:
           for (let i = 0; i < 3; i++) {
             if (data[0].data[i] === "a") {
               mbtiD.current.iCnt++;
+
+              //setMbtiState((cur) => ({ ...cur, iCnt: mbtiState.iCnt + 1 }));
             } else if (data[0].data[i] === "b") {
               mbtiD.current.eCnt++;
+              //setMbtiState((cur) => ({ ...cur, eCnt: mbtiState.eCnt + 1 }));
             }
           }
           break;
         case 1:
           for (let i = 0; i < 3; i++) {
-            if (data[0].data[i] === "a") {
+            if (data[1].data[i] === "a") {
               mbtiD.current.sCnt++;
-            } else if (data[0].data[i] === "b") {
+              //setMbtiState((cur) => ({...cur,sCnt: mbtiState.sCnt + 1,}));
+            } else if (data[1].data[i] === "b") {
               mbtiD.current.nCnt++;
+              //setMbtiState((cur) => ({...cur,nCnt: mbtiState.nCnt + 1,}));
             }
           }
           break;
         case 2:
           for (let i = 0; i < 3; i++) {
-            if (data[0].data[i] === "a") {
-              mbtiD.current.fCnt++;
-            } else if (data[0].data[i] === "b") {
+            if (data[2].data[i] === "a") {
               mbtiD.current.tCnt++;
+              //setMbtiState((cur) => ({...cur,fCnt: mbtiState.fCnt + 1,}));
+            } else if (data[2].data[i] === "b") {
+              mbtiD.current.fCnt++;
+              //setMbtiState((cur) => ({...cur,tCnt: mbtiState.tCnt + 1,}));
             }
           }
           break;
         case 3:
           for (let i = 0; i < 3; i++) {
-            if (data[0].data[i] === "a") {
+            if (data[3].data[i] === "a") {
               mbtiD.current.pCnt++;
-            } else if (data[0].data[i] === "b") {
+              //setMbtiState((cur) => ({...cur,pCnt: mbtiState.pCnt + 1,}));
+            } else if (data[3].data[i] === "b") {
               mbtiD.current.jCnt++;
+              //setMbtiState((cur) => ({..cur,jCnt: mbtiState.jCnt + 1,}));
             }
           }
           break;
         default:
       }
     }
+    console.log("after calculation");
+    console.log(mbtiD);
   };
 
+  const activate = () => {
+    console.log(counter);
+    counter === LAST_PAGE && getMbtiD(cntD.current);
+
+    counter === LAST_PAGE
+      ? navigate("/test/result", { state: mbtiD })
+      : //navigate("/test/result")
+        navigate(`/test/story/${counter + 1}`);
+    type === "a"
+      ? (cntD.current[Math.floor((counter - 1) / 3)].data[(counter - 1) % 3] =
+          "a")
+      : (cntD.current[Math.floor((counter - 1) / 3)].data[(counter - 1) % 3] =
+          "b");
+
+    console.log(cntD.current[Math.floor((counter - 1) / 3)].data);
+    console.log(counter, LAST_PAGE);
+    if (counter < LAST_PAGE) setCounter((cur) => cur + 1);
+  };
+  /*
   const activate = (id) => {
     // TODO: 점수 가산하기
     console.log(id);
     id === LAST_PAGE && getMbtiD(cntD.current);
     id === LAST_PAGE
       ? navigate("/test/result", { state: mbtiD })
-      : navigate(`/test/story/${parseInt(id) + 1}`);
+      : //navigate("/test/result")
+        navigate(`/test/story/${parseInt(id) + 1}`);
     type === "a"
       ? (cntD.current[Math.floor((id - 1) / 3)].data[(id - 1) % 3] = "a")
       : (cntD.current[Math.floor((id - 1) / 3)].data[(id - 1) % 3] = "b");
 
     console.log(cntD.current[Math.floor((id - 1) / 3)].data);
   };
+  */
   return (
-    <button
-      onClick={() => activate(id)}
-      className="w-full h-16 bg-gray-200 rounded-full"
-    >
-      {id !== LAST_PAGE ? decision : "나의 정체 확인하러 가기"}
+    <button onClick={activate} className="w-full h-16 bg-gray-300 rounded-full">
+      {counter !== LAST_PAGE ? decision : "나의 정체 확인하러 가기"}
     </button>
   );
 };
