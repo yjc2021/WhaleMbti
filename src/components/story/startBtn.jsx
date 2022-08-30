71accd7440c477e93410ed8b6fde0ecd5370a804
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-30 12:48:07 +0900

    💄 startBtn 컴포넌트 style 수정

--- src/components/story/startBtn.jsx
+++ src/components/story/startBtn.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const StartBtn = (props) => {
   const navigate = useNavigate();
   return (
-    <div className="flex justify-center">
-      <button
+    <div className="flex justify-center text-black ">
+      <ButtonWrapper
         onClick={() => navigate("/test/story/1")}
         className="w-1/3 h-16 bg-gray-200 rounded-full"
       >
         시작하기
-      </button>
+      </ButtonWrapper>
     </div>
   );
 };
 
 export default StartBtn;
+
+const ButtonWrapper = styled.button`
+  width: 100%;
+`;
